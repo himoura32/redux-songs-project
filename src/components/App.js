87bcc5d70590471778e68dff73bbe67444e04401
himoura32 <shawn.ahearn@outlook.com>
2022-06-01 16:57:36 -0400

    coded redux store/provider/connect/jsxList

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { selectSong } from '../actions';
+import SongList from './SongList';
+
+function App() {
+  return (
+    <div className='ui container grid'>
+      <div className='ui row'>
+        <div className='column eight wide'>
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
